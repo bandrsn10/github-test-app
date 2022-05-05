f7add92650e629a966914225bbbfd616fb9eb186
Ben Anderson <bandrsn10@gmail.com>
2022-05-04 22:20:23 -0400

    updated node-fetch to run tutorial code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,47 @@
+import fetch from 'node-fetch';
 
+type User = {
+    id: number;
+    email: string;
+    first_name: string;
+};
+
+type GetUsersResponse = {
+    data: User[];
+};
+
+async function getUsers(): Promise<any> {
+    try {
+        // 👇️ const response: Response
+        const response = await fetch('https://reqres.in/api/users', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        // 👇️ const result: GetUsersResponse
+        const result = (await response.json()) as GetUsersResponse;
+
+        console.log('result is: ', JSON.stringify(result, null, 4));
+
+        return result;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log('error message: ', error.message);
+            return error.message;
+        } else {
+            console.log('unexpected error: ', error);
+            return 'An unexpected error occurred';
+        }
+    }
+}
+
+getUsers();
 
 console.log('hey there!');
 console.log(process.env.GITHUB_TOKEN);
