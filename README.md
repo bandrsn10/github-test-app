05e88eb255bd693c590a3d0d99c21274617a0df5
Ben Anderson <bandrsn10@gmail.com>
2022-05-04 23:07:38 -0400

    cleaned up things

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # github-test-app
+
+## Links 
+[ Typescript API tutorial] (https://bobbyhadz.com/blog/typescript-http-request)
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,47 @@
 import fetch from 'node-fetch';
 
 const GITHUB_API_URL = 'https://api.github.com/'
-async function getRepositories(): Promise<any> {
+const ORG = 'RAMDA'
+
+type Repository = {
+    full_name: string;
+    name: string;
+}
+
+type GetRepositoryResponse = {
+    data: Repository[];
+  };
+
+async function main(): Promise<void> {  
+    const repositories = await getRepositories();
+    console.log(repositories);
+    console.log('hey there!');
+    console.log(process.env.GITHUB_TOKEN);
+    //set constant variable for Ramda org
+    
+    //call the API
+
+}
+
+async function getRepositories(): Promise<GetRepositoryResponse> {
     try {
         // 👇️ const response: Response
-        const response = await fetch(GITHUB_API_URL + 'users/defunkt', {
+        const response = await fetch(GITHUB_API_URL + 'orgs/'+ ORG + '/repos', {
             method: 'GET',
             headers: {
                 Accept: 'application/json',
             },
         });
+        
 
         if (!response.ok) {
             throw new Error(`Error! status: ${response.status}`);
         }
 
         // 👇️ const result: GetUsersResponse
-        const result = (await response.json());
-
-        console.log('result is: ', JSON.stringify(result, null, 4));
+        const result = (await response.json()) as GetRepositoryResponse;
+        
+        //console.log('result is: ', JSON.stringify(result, null, 4));
 
         return result;
     } catch (error) {
@@ -31,11 +54,5 @@ async function getRepositories(): Promise<any> {
         }
     }
 }
-
-getRepositories();
-
-console.log('hey there!');
-console.log(process.env.GITHUB_TOKEN);
-//set constant variable for Ramda org
-
-//call the API
+main();
+///repos/ramda/eslint-plugin-ramda/pulls?state=all
